2dff48356c0813024fd6e676d30bca6eafc113cc
Dmitry Brazhkin <dbrazhkin@gmail.com>
2018-07-13 14:25:30 +0500

    Add models: User, Location, Visit

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female')
+)
+
+
+class User(AbstractUser):
+    gender = models.CharField(max_length=1, null=True, choices=GENDER_CHOICES)
+    birth_date = models.DateField(null=True)
+    country = models.CharField(null=True)
+
+
+class Location(models.Model):
+    country = models.CharField()
+    city = models.CharField()
+    name = models.CharField()
+    description = models.CharField()
+
+
+class Visit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    ratio = models.SmallIntegerField(validators=(MinValueValidator(0), MaxValueValidator(10)))
